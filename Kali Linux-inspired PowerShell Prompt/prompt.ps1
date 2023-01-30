844a0848bb8ea36bb15a5014ff8d33f17625eea9
jerdub1993 <jerdub1993@gmail.com>
2023-01-29 18:16:08 -0800

    updates

--- Kali Linux-inspired PowerShell Prompt/prompt.ps1
+++ Kali Linux-inspired PowerShell Prompt/prompt.ps1
@@ -11,11 +11,12 @@ function prompt {
     $Dash = [char]9472
 
     # Full color list: https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#text-formatting
-    $LineColor   = "[92m"   # Bright Green
-    $BaseColor   = "[94m"   # Bright Blue
-    $AccentColor = "[0m"    # Uncolored
-    $PWDColor    = $AccentColor
-    $RepoColor   = $BaseColor
+    $LineColor    = "[92m"   # Bright Green
+    $BaseColor    = "[94m"   # Bright Blue
+    $AccentColor  = "[0m"    # Uncolored
+    $BracketColor = "[93m"    # Uncolored
+    $PWDColor     = $AccentColor
+    $RepoColor    = $BaseColor
 
     # If current directory is a Git repository, add "@reponame" to the prompt
     $RepoString = try {
@@ -32,6 +33,6 @@ function prompt {
         $env:USERPROFILE {"~"}
         Default {$_}
     }
-    $String = "`n{0}{1}{5}{7}{7}{0}{2}({0}{3}{8}{10}{0}{2})-[{0}{4}{9}{0}{2}]`n{0}{1}{6}{7}{0}{3}`${0}{2} " -f $ESC, $LineColor, $AccentColor, $BaseColor, $PWDColor, $RtAngleDown, $RtAngleUp, $Dash, '{0}', '{1}', $RepoString
+    $String = "`n{0}{1}{6}{8}{8}{0}{5}({0}{3}{9}{11}{0}{5}){0}{2}-{0}{5}[{0}{4}{10}{0}{5}]`n{0}{1}{7}{8}{0}{3}`${0}{2} " -f $ESC, $LineColor, $AccentColor, $BaseColor, $PWDColor, $BracketColor, $RtAngleDown, $RtAngleUp, $Dash, '{0}', '{1}', $RepoString
     $String -f $env:USERNAME, $PWDPath
 }
