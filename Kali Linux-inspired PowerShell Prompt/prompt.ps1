c00f6356293b897de44c4fc4705eb22aeded32ed
jerdub1993 <jerdub1993@gmail.com>
2023-01-29 18:33:24 -0800

    updates

--- Kali Linux-inspired PowerShell Prompt/prompt.ps1
+++ Kali Linux-inspired PowerShell Prompt/prompt.ps1
@@ -5,10 +5,10 @@ function prompt {
         ┌──(<username><@repository>)-[<Present Working Directory>]
         └─$ 
     #>
-    $ESC = [char]27
-    $RtAngleDown = [char]9484
-    $RtAngleUp = [char]9492
-    $Dash = [char]9472
+    $ESC             = [char]27
+    $RtAngleDown     = [char]9484
+    $RtAngleUp       = [char]9492
+    $Dash            = [char]9472
 
     # Full color list: https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#text-formatting
     $LineColor    = "[92m"   # Bright Green
@@ -30,7 +30,7 @@ function prompt {
 
     # Replace the user's profile directory in path with "~"
     $PWDPath = $pwd.path -replace $env:USERPROFILE.replace('\','\\'), "~"
-    
+
     $String = "`n{0}{1}{6}{8}{8}{0}{5}({0}{3}{9}{11}{0}{5}){0}{2}-{0}{5}[{0}{4}{10}{0}{5}]`n{0}{1}{7}{8}{0}{3}`${0}{2} " -f $ESC, $LineColor, $AccentColor, $BaseColor, $PWDColor, $BracketColor, $RtAngleDown, $RtAngleUp, $Dash, '{0}', '{1}', $RepoString
     $String -f $env:USERNAME, $PWDPath
 }
