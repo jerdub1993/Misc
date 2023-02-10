4262425cad8331bddbc63979ccb5f78f71d55681
Jeremiah S. Watkins (CTR) <jeremiah.s.watkins.ctr@us.navy.mil>
2023-02-09 17:32:36 -0800

    major updates

--- Generate Cmdlet Documentation/New-CmdletDocumentation.ps1
+++ Generate Cmdlet Documentation/New-CmdletDocumentation.ps1
@@ -2,7 +2,7 @@ function New-CmdletDocumentation {
     param (
         [Parameter(
             Mandatory = $true,
-            ParameterSetName = 'Cmdlet',
+            ParameterSetName = 'InputObject',
             ValueFromPipeline = $true
         )]    
         [Object[]]$InputObject,
@@ -11,21 +11,50 @@ function New-CmdletDocumentation {
             ParameterSetName = 'Name'
         )]    
         [string[]]$Name,
+        [Parameter(
+            Mandatory = $true,
+            ParameterSetName = 'Script'
+        )]    
+        [string[]]$Script,
         [switch]$LoremIpsum
     )
     Begin {
-        foreach ($Obj in $InputObject){
-            if ($Obj.GetType() -notin [System.Management.Automation.CmdletInfo],[System.Management.Automation.FunctionInfo]){
-                throw [System.Management.Automation.ParameterBindingException] "Cannot process argument transformation on parameter 'InputObject'. Cannot convert the `"$Obj`" value of type `"$($Obj.GetType().Name)`" to type `"CmdletInfo`" or `"FunctionInfo`"."
-                exit 1
+        function Split-Link {
+            param (
+                [string]$Text
+            )
+            $Split = $Text -split ': '
+            if ($Text -match '^.*: https?:\/\/(.+\.)+.+\/?[^\s\t\n\r]*$' -and [System.Uri]::IsWellFormedUriString($Split[1], [System.UriKind]::Absolute)) {
+                return [PSCustomObject]@{
+                    Label   = $Split[0].TrimEnd(':')
+                    Uri     = $Split[1]
+                }
             }
         }
-        if ($Name){
-            try {
-                $InputObject = Get-Command -Name $Name
-            } catch {
-                throw [System.Management.Automation.CommandNotFoundException] "Unable to find command '$Name'."
-                exit 1
+        switch ($PSCmdlet.ParameterSetName){
+            InputObject {
+                foreach ($Obj in $InputObject){
+                    if ($Obj.GetType() -notin [System.Management.Automation.CmdletInfo],[System.Management.Automation.FunctionInfo]){
+                        throw [System.Management.Automation.ParameterBindingException] "Cannot process argument transformation on parameter 'InputObject'. Cannot convert the `"$Obj`" value of type `"$($Obj.GetType().Name)`" to type `"CmdletInfo`" or `"FunctionInfo`"."
+                        exit 1
+                    }
+                }
+            }
+            Name        {
+                try {
+                    $InputObject = Get-Command -Name $Name
+                } catch {
+                    throw [System.Management.Automation.CommandNotFoundException] "Unable to find command '$Name'."
+                    exit 1
+                }
+            }
+            Script      {
+                try {
+                    $InputObject = (Get-Item -LiteralPath $Script).FullName
+                } catch {
+                    throw [System.Management.Automation.ItemNotFoundException] "Cannot find path '$($PWD.Path)\$Script' because it does not exist."
+                    exit 1
+                }
             }
         }
         $Required_Modules = @{
@@ -57,58 +86,102 @@ function New-CmdletDocumentation {
         foreach ($Obj in $InputObject){
             $Help = Get-Help $Obj.Name
             $OutArray = @()
-            $OutArray += "# {0}`n" -f $Obj.Name
-            $OutArray += if ($LoremIpsum){
+
+            #region Cmdlet Name
+            $OutArray += "# {0}`n" -f $Help.Name
+            #endregion Cmdlet Name
+
+            #region Synopsis
+            $OutArray += if ($Help.Synopsis) {
+                $Help.Synopsis.Trim()
+            } elseif ($LoremIpsum){
                 Get-LoremIpsum -Sentences 1
-            } else {
-                ""
             }
-            $OutArray += "## Syntax`n"
-            $Syntax = Get-CommandSyntax -Command $Obj
+            #endregion Synopsis
+
+            #region Syntax
+            $OutArray += "`n## Syntax"
             $spaces = ' ' * 4
+            $Syntax = Get-CommandSyntax -Command $Obj
             foreach ($set in $Syntax.ParameterSets){
-                $OutArray += "{0}{1}" -f $spaces, $Obj.Name
+                $OutArray += '```PowerShell'
+                $OutArray += $Help.Name
                 foreach ($param in $set.Parameters){
-                    $OutArray += "{0}{1}" -f ($spaces * 2), $param
+                    $OutArray += "{0}{1}" -f $spaces, $param
                 }
-                $OutArray += ''
+                $OutArray += '```'
             }
-            $OutArray += "## Description`n"
-            
+            #endregion Syntax
+
+            #region Description
+            $OutArray += "`n## Description"
             $OutArray += if ($Help.description){
                 $Help.description
             } elseif ($LoremIpsum){
                 Get-LoremIpsum -Paragraphs 2
-            } else {
-                ""
             }
-            $OutArray += "## Examples`n"
+            #endregion Description
+
+            #region Examples
+            $OutArray += "`n## Examples"
             $exCount = 1
             foreach ($example in $Help.examples.example){
-                $OutArray += "### {0}`n" -f $example.title.trim('-').trim() -replace 'Example \d+', ('Example {0}' -f $exCount)
+                $OutArray += "`n### Example {0}" -f $exCount
+                $OutArray += '```PowerShell'
                 foreach ($line in ($example.code -split "`n")){
-                    $OutArray += "{0}{1}" -f $spaces, $line
+                    $OutArray += $line
                 }
-                $OutArray += $example.remarks
+                $OutArray += '```'
+                $OutArray += if ($example.remarks){
+                    foreach ($line in (($example.remarks.text | Where-Object {![string]::IsNullOrEmpty($_.Trim())}) -split "`n")){
+                        "`n$line"
+                    }
+                } elseif ($LoremIpsum){
+                    Get-LoremIpsum -Sentences 2
+                }
                 $exCount++
             }
-            $OutArray += "## Parameters`n"
+            #endregion Examples
+
+            #region Parameters
+            $OutArray += "`n## Parameters"
+            $htmlSpaces = '&ensp;' * 4
+            $tablePattern = "| {0} | {1} |"
             foreach ($param in $Help.Parameters.parameter){
-                $OutArray += "#### **`-{0}`**`n" -f $param.name
-                $OutArray += $param.Description
+                $OutArray += "`n#### **`-{0}`**" -f $param.name
+                $OutArray += if ($param.Description){
+                    "{0}{1}" -f $htmlSpaces, $param.Description.Text
+                } elseif ($LoremIpsum) {
+                    "{0}{1}" -f $htmlSpaces, (Get-LoremIpsum -Sentences 1)
+                }
                 $OutArray += ''
-                $OutArray += "| Attribute | Value |"
-                $OutArray += "| --- | --- |"
-                $OutArray += "| Type | {0} |" -f $param.type.name
-                $OutArray += "| Position | {0} |" -f $param.position
-                $OutArray += "| AcceptPipelineInput | {0} |" -f [System.Convert]::ToBoolean($param.pipelineInput.split()[0])
+                $OutArray += $tablePattern -f "Attribute", "Value"
+                $OutArray += $tablePattern -f "---", "---"
+                $TypeName = if ($param.type.name -eq 'switch'){
+                    'SwitchParameter'
+                } else {
+                    $param.type.name
+                }
+                $OutArray += $tablePattern -f "Type", $TypeName
+                $OutArray += $tablePattern -f "Position", (Get-Culture).TextInfo.ToTitleCase($param.position)
+                $DefaultValue = if ($param.type.name -eq 'switch') {
+                    $false
+                } elseif ($param.defaultValue) {
+                    $param.defaultValue.Trim()
+                } else {
+                    "None"
+                }
+                $OutArray += $tablePattern -f "Default value", $DefaultValue
+                $OutArray += $tablePattern -f "AcceptPipelineInput", [System.Convert]::ToBoolean($param.pipelineInput.split()[0])
             }
+            #endregion Parameters
+
+            #region Inputs
             $OutArray += "## Inputs`n"
-            $inputText = foreach ($inputType in $Help.inputTypes.inputType){
-                $Description = $inputType.description
-                foreach ($indvType in ($inputType.type.name.split("`n").split(",").trim() | Where-Object {![string]::IsNullOrEmpty($_.trim())})){
-                    "#### [**{0}**]()`n" -f $indvType
-                    $Description
+            $inputText = foreach ($inputType in ($Help.parameters.parameter.type.name | Select-Object -Unique | Sort-Object)){
+                "#### [**{0}**]()`n" -f $inputType
+                if ($LoremIpsum) {
+                    Get-LoremIpsum -Sentences 1
                 }
             }
             $OutArray += if ($inputText.Count -gt 0){
@@ -116,11 +189,16 @@ function New-CmdletDocumentation {
             } else {
                 $OutArray += "#### **None**`n"
             }
+            #endregion Inputs
+
+            #region Outputs
             $OutArray += "## Outputs"
             $returnValues = foreach ($returnValue in $Help.returnValues.returnValue){
-                "#### [**{0}**]()`n" -f $returnValue.type.name
+                "#### [**{0}**]()`n" -f $returnValue.type.name.Trim()
                 if ($returnValue.Description){
                     $returnValue.Description
+                } elseif ($LoremIpsum){
+                    Get-LoremIpsum -Sentences 1
                 }
             }
             $OutArray += if ($returnValues.Count -eq 0){
@@ -128,21 +206,58 @@ function New-CmdletDocumentation {
             } else {
                 $returnValues
             }
+            #endregion Outputs
+
+            #region Notes
             $OutArray += "## Notes`n"
-            $OutArray += if ($LoremIpsum){
+            $aliasList = Get-Alias -Definition $Help.Name -ErrorAction SilentlyContinue
+            $OutArray += if ($aliasList) {
+                'PowerShell includes the following aliases for `{0}`:' -f $Help.Name
+                foreach ($alias in $aliasList){
+                    '- `{0}`' -f $alias.Name
+                }
+            } elseif ($LoremIpsum){
                 Get-LoremIpsum -Paragraphs 3
-            } else {
-                ""
             }
+            #endregion Notes
+
+            #region Related Links
             $OutArray += "## Related Links`n"
-            $URIList = $help.relatedLinks.navigationLink | Where-Object { $_.uri -and ![string]::IsNullOrEmpty($_.uri.trim()) }
-            if ($URIList.Count -gt 0){
-                foreach ($URI in $URIList){
-                    $OutArray += "- [{0}]({1})" -f $URI.linkText, $URI.uri
+            $LinkPattern = '- [{0}]({1})'
+            $Links = $help.relatedLinks.navigationLink
+            if ($Links){
+                $defaultUri = $true
+                foreach ($uri in ($Links.Uri | Where-Object {![string]::IsNullOrEmpty($_)})){
+                    $OutArray += switch ($defaultUri){
+                        $true   {
+                            $LinkPattern -f $Help.Name, $uri
+                        }
+                        $false  {
+                            $LinkPattern -f $uri, $uri
+                        }
+                    }
+                    $defaultUri = $false
+                }
+                if ($Links.linkText){
+                    foreach ($linkText in ($Links.linkText | Where-Object {![string]::IsNullOrEmpty($_)})){
+                        $splitLink = Split-Link -Text $linkText
+                        $cmd = Get-Command -Name $linkText -ErrorAction SilentlyContinue
+                        $OutArray += if ([System.Uri]::IsWellFormedUriString($linkText, [System.UriKind]::Absolute)){
+                            $LinkPattern -f $linkText, $linkText
+                        } elseif ($splitLink){
+                            $LinkPattern -f $splitLink.Label, $splitLink.Uri
+                        } elseif ($cmd -and $cmd.helpUri) {
+                            $LinkPattern -f $linkText, $cmd.helpUri
+                        } else {
+                            "- $linkText"
+                        }
+                    }
                 }
             } else {
-                $OutArray += "- [Link 1]()"
+                $OutArray += "- [$(Get-LoremIpsum -Words 2)]()"
             }
+            #endregion Related Links
+
             return $OutArray
         }
     }
